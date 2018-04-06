36942344c5e5fcb6ef31ce2ba746442f9f6cf165
Gert Hengeveld <info@ghengeveld.nl>
2018-04-06 11:34:00 +0200

    Render some JSX to HTML from AWS Lambda.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 class LambdaDemo extends Component {
   constructor(props) {
-    super(props);
-    this.state = {loading: false, msg: null};
+    super(props)
+    this.state = { loading: false, html: null }
   }
 
-  handleClick = (e) => {
-    e.preventDefault();
+  handleClick = e => {
+    e.preventDefault()
 
-    this.setState({loading: true});
-    fetch('/.netlify/functions/hello')
-      .then(response => response.json())
-      .then(json => this.setState({loading: false, msg: json.msg}));
+    this.setState({ loading: true })
+    fetch("/.netlify/functions/hello")
+      .then(response => response.text())
+      .then(html => this.setState({ loading: false, html: html }))
   }
 
   render() {
-    const {loading, msg} = this.state;
+    const { loading, html } = this.state
 
-    return <p>
-      <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
-      <span>{msg}</span>
-    </p>
+    return (
+      <p>
+        <button onClick={this.handleClick}>{loading ? "Loading..." : "Call Lambda"}</button>
+        <br />
+        <span dangerouslySetInnerHTML={{ __html: html }} />
+      </p>
+    )
   }
 }
 
@@ -38,10 +41,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <LambdaDemo/>
+        <LambdaDemo />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,7 +1,13 @@
+const React = require("react")
+const ReactDOMServer = require("react-dom/server")
+
 export function handler(event, context, callback) {
   console.log(event)
+
+  const body = ReactDOMServer.renderToString(<div>Hello from JSX</div>)
+
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({msg: "Hello, World!"})
+    body
   })
 }
